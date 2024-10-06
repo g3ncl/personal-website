e24c4bd703555888529b1c2443b837462cb51f3a
Claudio Genovese <18346630+g3ncl@users.noreply.github.com>
2024-10-06 21:18:14 +0200

    Moved from netlify redirect to Next.js rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+/* eslint-disable prefer-arrow/prefer-arrow-functions */
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/snake4d",
+        destination: "https://snake4d.netlify.app",
+      },
+      {
+        source: "/snake4d/:path*",
+        destination: "https://snake4d.netlify.app/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
